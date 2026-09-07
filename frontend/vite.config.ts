0e7f4f7b17f0119c858be5d261372c044acb1a6f
xylanny <lqj04@outlook.com>
2026-09-07 20:06:04 +0800

    chore: 配置在启动前端开发服务器时，直接打开浏览器预览；修改页签名

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+});
